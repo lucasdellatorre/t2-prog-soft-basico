ca730ee35a205f688f490cfb57d4e4323adb03bc
lucasdellatorre <lucasdellatorre222@gmail.com>
2022-05-28 14:25:21 -0300

    versao inicial para flag -h

--- src/main.c
+++ src/main.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <unistd.h>
 
-int main(int argc, char **argv) {
-    printf("hello");
+int main 
+(int argc, char **argv) 
+{
+    char ch;
+    while ( (ch = getopt(argc, argv, "h")) != EOF)
+    {
+        switch (ch)
+        {
+            case 'h':
+                help();
+                break;
+        }
+    }
+
+    argc -= optind;
+    argv += optind;
+}
+
+void help()
+{
+    printf("essa eh a saida para flag help")
 }
