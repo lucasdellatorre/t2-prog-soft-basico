f2891cfe1295524fd69a8a44a9818b1deb83f0fe
lucasdellatorre <lucasdellatorre222@gmail.com>
2022-06-10 19:55:46 -0300

    formatando o codigo

--- main.c
+++ main.c
@@ -7,8 +7,9 @@
 
 #define MAX_LEN 1000
 
-int main(int argc, char * const argv[]) {
-  Palavra *list_word = NULL; 
+int main(int argc, char *const argv[])
+{
+  Palavra *list_word = NULL;
   FILE *txt_input = fopen(argv[2], "r");
   char line[MAX_LEN];
   int pcount = 1;
@@ -17,62 +18,75 @@ int main(int argc, char * const argv[]) {
   int option;
   int vflag = 0;
   int hflag = 0;
-  while((option = getopt(argc, argv, "vh")) != -1) {
-    switch (option) {
-      case 'v':
-              if(vflag) {
-                printf("Only one option allowed\n");
-                exit(1);
-              } else {
-                vflag++;
-                hflag++;
-              }
-              printf("Indice Remissivo V1.0");
-              break;
-      case 'h':
-              if(hflag) {
-                printf("Only one option allowed\n");
-                exit(1);
-              } else {
-                vflag++;
-                hflag++;
-              }
-              printf("Usage: ./main.out exit.csv caso.txt palavras.html");
-              break;
+  while ((option = getopt(argc, argv, "vh")) != -1)
+  {
+    switch (option)
+    {
+    case 'v':
+      if (vflag)
+      {
+        printf("Only one option allowed\n");
+        exit(1);
+      }
+      else
+      {
+        vflag++;
+        hflag++;
+      }
+      printf("Indice Remissivo V1.0");
+      break;
+    case 'h':
+      if (hflag)
+      {
+        printf("Only one option allowed\n");
+        exit(1);
+      }
+      else
+      {
+        vflag++;
+        hflag++;
+      }
+      printf("Usage: ./main.out exit.csv caso.txt palavras.html");
+      break;
     }
   }
 
-  if(txt_input == NULL) {
+  if (txt_input == NULL)
+  {
     printf("error in opening the file\n");
     return 0;
   }
-  
-  while(fgets(line, sizeof line, txt_input) != NULL) {
+
+  while (fgets(line, sizeof line, txt_input) != NULL)
+  {
     char *token = strtok(line, " ");
-  
+
     // faz um split das palavras da linha
-    while(token != NULL) {
-      
-      //printf("%s\n", token);
+    while (token != NULL)
+    {
+
+      // printf("%s\n", token);
       limpaString(token);
       list_word = inserePalavra(token, list_word, pcount, &count_words);
       token = strtok(NULL, " ");
     }
-  
+
     // confere se há uma sequencia de '\n' '\n', (paragrafo)
-    if(line[0] == '\n') {
-      if(temp == 1)
+    if (line[0] == '\n')
+    {
+      if (temp == 1)
         pcount++;
       temp = 0;
       continue;
     }
-    else {
+    else
+    {
       temp = 1;
     }
   }
-  
+
   printa(list_word, argv[1]);
-  
+
   printf("\n");
 
   printaHTML(argv[1], argv[3]);
--- palavra.c
+++ palavra.c
@@ -7,39 +7,45 @@
 
 typedef struct typeword Palavra;
 
-struct typeword{
+struct typeword
+{
   char string[SIZE_WORDS];
   int array[20]; // array que contém os parágrafos que a palavra apareceu
-  int len; // contador de incidências de parágrafos
-  int cont; // contador de incidências de palavras
-  Palavra* next; // ponteiro para a próxima palavra
+  int len;       // contador de incidências de parágrafos
+  int cont;      // contador de incidências de palavras
+  Palavra *next; // ponteiro para a próxima palavra
 };
 
 // coloca a string em minúsculo e limpa ela de tudo que não for letra minúscula
-void limpaString(char *word){
-  
+void limpaString(char *word)
+{
+
   int index = strlen(word) - 1;
 
   int i;
 
-  for(i=index;i>0;i--){
-    word[0] = (word[0] > 64 && word[0] < 91) ? word[0]+32 : word[0];
-      if(word[i]<97 || word[i]>122){
-          word[i]='\0';
-      }
+  for (i = index; i > 0; i--)
+  {
+    word[0] = (word[0] > 64 && word[0] < 91) ? word[0] + 32 : word[0];
+    if (word[i] < 97 || word[i] > 122)
+    {
+      word[i] = '\0';
+    }
   }
   return;
 }
 
 // verifica se adiciona uma nova palavra ou se somente adiciona o parágrafo onde está
-Palavra* inserePalavra(char *word, Palavra *list, int paragraph, int *count_words){
+Palavra *inserePalavra(char *word, Palavra *list, int paragraph, int *count_words)
+{
   Palavra *i;
 
-  if(list == NULL){
-    
+  if (list == NULL)
+  {
+
     // aloca memória
-    Palavra* first_word;
-    first_word = (Palavra*)malloc(sizeof(Palavra));
+    Palavra *first_word;
+    first_word = (Palavra *)malloc(sizeof(Palavra));
 
     strcpy(first_word->string, word);
     first_word->array[0] = paragraph; // adiciona o paragrafo em que a palavra está
@@ -50,39 +56,43 @@ Palavra* inserePalavra(char *word, Palavra *list, int paragraph, int *count_word
     return list;
   }
 
-  i  = list;
+  i = list;
   Palavra *list_end;
 
   // percorre a lista para ver se a palavra já foi adicionada
-  while(i!=NULL){
-    
-    if(strcmp(i->string, word) == 0){
-      
+  while (i != NULL)
+  {
+
+    if (strcmp(i->string, word) == 0)
+    {
+
       i->cont++;
-      
-      // se a posicao anterior do array contém o mesmo numero do paragrafo, entao está 
+
+      // se a posicao anterior do array contém o mesmo numero do paragrafo, entao está
       // repetido, logo não precisa inserir no array
-      if (i->array[i->len - 1] == paragraph) {
-        return list; 
+      if (i->array[i->len - 1] == paragraph)
+      {
+        return list;
         i->cont = i->cont + 1;
       }
-      
+
       i->array[i->len] = paragraph; // adiciona o numero do paragrafo no array
-      i->len = i->len + 1; // incrementa o contador de incidências de parágrafos
+      i->len = i->len + 1;          // incrementa o contador de incidências de parágrafos
       return list;
     }
-    
-    if(i->next==NULL) {
-        list_end = i;
+
+    if (i->next == NULL)
+    {
+      list_end = i;
     }
-    
+
     i = i->next;
   }
 
   // se não achou a palavra na lista, aloca memória para uma nova palavra
   Palavra *new_word;
-  new_word = (Palavra*)malloc(sizeof(Palavra));
-  
+  new_word = (Palavra *)malloc(sizeof(Palavra));
+
   strcpy(new_word->string, word);
   new_word->cont = 1;
   new_word->array[0] = paragraph;
@@ -93,39 +103,47 @@ Palavra* inserePalavra(char *word, Palavra *list, int paragraph, int *count_word
   return list;
 }
 
-void printa(Palavra *list, const char* arquivo) {
+void printa(Palavra *list, const char *arquivo)
+{
 
   FILE *output = fopen(arquivo, "w");
-  Palavra* aux;
-  if(list == NULL) {
+  Palavra *aux;
+  if (list == NULL)
+  {
     printf("Lista vazia");
-  } else {
-      for(aux = list; aux != NULL; aux = aux->next) {
-        // printa a palavra
-        fprintf(output, "%s; | ", aux->string);
-        for(int i = 0; i < aux->len; i++) {
-          // printa todas as ocorrencias em paragrafos
-          fprintf(output, "%d | ", aux->array[i]);
-        }
-        fprintf(output, "\n");
+  }
+  else
+  {
+    for (aux = list; aux != NULL; aux = aux->next)
+    {
+      // printa a palavra
+      fprintf(output, "%s; | ", aux->string);
+      for (int i = 0; i < aux->len; i++)
+      {
+        // printa todas as ocorrencias em paragrafos
+        fprintf(output, "%d | ", aux->array[i]);
       }
+      fprintf(output, "\n");
     }
+  }
   fclose(output);
 }
 
 // pega campos do csv
-const char* getfield(char* line, int num)
+const char *getfield(char *line, int num)
 {
-  const char* tok;
-  for (tok = strtok(line, ";"); tok && *tok; tok = strtok(NULL, ";\n")) {
+  const char *tok;
+  for (tok = strtok(line, ";"); tok && *tok; tok = strtok(NULL, ";\n"))
+  {
     if (!--num)
       return tok;
   }
   return NULL;
 }
 
-void printaHTML (const char* saida, const char* html) {
-  FILE* exit = fopen(saida, "r");
+void printaHTML(const char *saida, const char *html)
+{
+  FILE *exit = fopen(saida, "r");
   FILE *txt_output_2 = fopen(html, "w");
   char line2[100000];
 
@@ -146,15 +164,16 @@ void printaHTML (const char* saida, const char* html) {
   <th> Paragrafo </th> \
   </tr> \
   <tr>");
- 
+
   // copia linha e encontra os campos divididos por ;
-  while (fgets(line2, 100000, exit)) {
-    char* tmp = strdup(line2);
-    fprintf(txt_output_2,"<tr><td>%s</td><td>%s</td></tr>", getfield(tmp, 1), getfield(tmp, 2));
+  while (fgets(line2, 100000, exit))
+  {
+    char *tmp = strdup(line2);
+    fprintf(txt_output_2, "<tr><td>%s</td><td>%s</td></tr>", getfield(tmp, 1), getfield(tmp, 2));
     // NOTE strtok clobbers tmp
     free(tmp);
   }
-  
+
   fprintf(txt_output_2, "</tr> \
   </table> \
   </body> \
